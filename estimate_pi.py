8d212166bea371d76e7fd42011796d054f3ebab9
WinThantAung <winthantag1998@gmail.com>
2025-12-17 14:36:56 +0100

    'done'

--- estimate_pi.py
+++ estimate_pi.py
@@ -1,7 +1,15 @@
+
+import numpy as np
+
 def estimate_pi(num_samples):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    inside_count = 0
+
+    for _ in range(num_samples):
+        x = np.random.rand()
+        y = np.random.rand()
+
+        if x ** 2 + y ** 2 <= 1:
+            inside_count += 1
+
+    pi_estimate = 4 * inside_count / num_samples
+    return pi_estimate
--- get_random_subset_of_naturals_up_to_20.py
+++ get_random_subset_of_naturals_up_to_20.py
@@ -1,9 +1,14 @@
+
+import numpy as np
+
 def get_random_subset_of_naturals_up_to_20():
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    numbers = np.arange(1, 21)
+
+    mask = np.random.randint(0, 2, size=20)
+    subset = numbers[mask == 1]
+
+    return subset
+
 
 
